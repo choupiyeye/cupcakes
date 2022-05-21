86e43a2c39538b30946655deabbbb245054c2d84
choupi <choupiyeye@gmail.com>
2022-05-21 14:08:38 +0200

    resolve bug in script.js & contact.html

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-"Merci de nous avoir envoyé un message. Je ne pourrais pas y répondre.";
+/*"Merci de nous avoir envoyé un message. Je ne pourrais pas y répondre.";
 
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    alert(message);
+  }); */
+  const message = "Merci de nous avoir envoyé un message. Je ne pourrais pas y répondre.";
+  const contactForm= document.getElementById("contactForm");
+  contactForm.addEventListener("submit", function (event) {event.preventDefault();
     alert(message);
   });
